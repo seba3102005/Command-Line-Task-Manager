1282c685d5fc246c65a99b2781ec94713185715d
seba ibraheem <sebaalmahdy@gmail.com>
2024-10-30 22:57:33 +0300

    menu and validation was added

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello")
+def MenuShow ():
+    print("1. View all tasks")
+    print("2. Add a task")
+    print("3. Update a task")
+    print("4. Delete a task")
+    print("5. Exit")
+    print("======================")
+    print("Please choose an option from 1-5")
+
+MenuShow()
+choice = input("Enter your choice: ")
+choices = ['1','2','3','4','5']
+while choice not in choices:
+    print("Invalid choice ,Please Enter a valid one:")
+    MenuShow()
+    choice = input("Enter your choice: ")
+
+else:
+    pass
+
