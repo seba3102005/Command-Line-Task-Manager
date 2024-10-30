4d663e850828e5241205d72ab4d40f662779b5b6
seba ibraheem <sebaalmahdy@gmail.com>
2024-10-30 23:33:38 +0300

    the add and show function of the tasks were made

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+description = {}
+priority = {}
+due_date = {}
+
 def MenuShow ():
     print("1. View all tasks")
     print("2. Add a task")
@@ -7,14 +11,42 @@ def MenuShow ():
     print("======================")
     print("Please choose an option from 1-5")
 
-MenuShow()
-choice = input("Enter your choice: ")
-choices = ['1','2','3','4','5']
-while choice not in choices:
-    print("Invalid choice ,Please Enter a valid one:")
-    MenuShow()
-    choice = input("Enter your choice: ")
+def Add_task ():
+    name = "Task"+str((len(description)+1))
+    desc = input("Please Enter theTask descripyion")
+    prio = input("Please enter the Priority of your task")
+    date = input("enter the task's due date")
 
-else:
-    pass
+    description.setdefault( name , desc )
+    priority.setdefault(name , prio )
+    due_date.setdefault(name , date )
+
+def show_tasks():
+    i=1
+    for x,y,z in zip(description.values(),priority.values(),due_date.values()):
+        print (f"Task {i}: {x} , the priority of it is {y} ,the DUE DATE is on {z}")
+        i+=1
+
+
+
+
+
+MenuShow()
+while (True):
+    choice = input("Enter your choice: ")
+    choices = ['1','2','3','4','5']
+    while choice not in choices:
+       print("Invalid choice ,Please Enter a valid one:")
+       MenuShow()
+       choice = input("Enter your choice: ")
+    if(choice=='5'):
+        break
+    
+    if(choice =='2'):
+        Add_task()
+    elif (choice=='1'):
+        show_tasks()
+
+    
+
 
